e911a8c7d5965bb8fa7051f7400b87b86b9f58ea
Neil Ranada <neil.ranada@gmail.com>
2022-11-20 22:07:50 -0800

    Update vertex and edge methods

--- src/lib/Graph.ts
+++ src/lib/Graph.ts
@@ -8,4 +8,19 @@ export class Graph {
             this.adjacencyList[vertex] = [];
         }
     }
+    addEdge(vertex1: string, vertex2: string) {
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+    removeEdge(vertex1: string, vertex2: string) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+    }
+    removeVertex(vertex: string) {
+        while (this.adjacencyList[vertex].length) {
+            const poppedAdjacentVertex = this.adjacencyList[vertex].pop();
+            if (poppedAdjacentVertex) this.removeEdge(vertex, poppedAdjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+    } 
 }
