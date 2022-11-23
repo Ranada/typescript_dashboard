09403675c5a1df80b105ba1ffe0b9c5002731125
Neil Ranada <neil.ranada@gmail.com>
2022-11-22 21:40:19 -0800

    Import express

---
+++ dist/server.js
@@ -0,0 +1,14 @@
+"use strict";
+// Import express and create app
+const express = require('../node_modules/express');
+const app = express();
+// Process environment variables
+require('../node_modules/dotenv').config();
+// Instantiate database
+// Middleware for parsing request body
+app.use(express.json());
+// Routes
+// Set port and listen for incoming requests
+const port = 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+//# sourceMappingURL=server.js.map
